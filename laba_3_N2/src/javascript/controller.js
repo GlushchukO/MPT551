554aefc54d048ec83641e294f1aabfb0b456f18f
unknown <glushchukolya@gmail.com>
2019-05-14 17:45:19 +0300

    laba_3_N2

---
+++ laba_3_N2/src/javascript/controller.js
@@ -0,0 +1,18 @@
+var controller = {
+
+		init: function () {
+			view.showQuestion(model.getCurrentQuestion());
+		},
+
+		handleClick: function () {
+			var mainInput = document.getElementById('mainInput').value;
+			var result = model.calculate(mainInput);
+			if (result.ok){
+				model.goToNextQuestion();
+				view.showQuestion(model.getCurrentQuestion());
+			} else {
+				view.showError(result.errorText);
+			}
+		}
+
+	};
---
+++ laba_3_N2/src/javascript/event.js
@@ -0,0 +1,23 @@
+(function() { 
+
+		var app = {
+
+			init: function () {
+				this.main();
+				this.event();
+			},
+
+			main: function () {
+				controller.init();
+			},
+
+			event: function () {
+				var el = document.getElementById("calcUser");
+				el.onclick = controller.handleClick;
+			}
+
+		};
+
+		app.init();
+
+	}());
